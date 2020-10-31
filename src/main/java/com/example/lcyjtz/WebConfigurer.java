518dcae6d6b4b56a20815de87afbe22490e5fe30
lcyjtz <lcyjtz@qq.com>
2020-10-31 15:32:12 +0800

    自定义页面打开

---
+++ src/main/java/com/example/lcyjtz/WebConfigurer.java
@@ -0,0 +1,19 @@
+package com.example.lcyjtz;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        //默认地址（可以是页面或后台请求接口）
+        registry.addViewController("/").setViewName("forward:/index.html");
+        //设置过滤优先级最高
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
